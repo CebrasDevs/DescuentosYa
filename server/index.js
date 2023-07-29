5f2826461dffa6c7d055382dd258ef041b02b790
SnowDevLC <canalesluis9@gmail.com>
2023-07-29 20:20:48 -0300

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+require('dotenv').config();
+const { PORT } = process.env;
+
+conn.sync({ force: true }).then(() => {
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+})
+}).catch(error => console.error(error))
