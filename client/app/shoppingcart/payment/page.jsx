b655dcb5eab75107f491cede3e2a1e0678054945
misaelc <misaelclemente98@gmail.com>
2023-07-31 17:52:07 -0300

    feat(client): Added View Files

---
+++ client/app/shoppingcart/payment/page.jsx
@@ -0,0 +1,8 @@
+export default function payment() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500  text-9xl  " >I AM A PAYMENT</h1>
+      </main>
+    )
+  }
+  
