1ccd98716fd793fef10011a2bbb6eca4b7508b84
Pablo Bestani <pablobestanimouk@gmail.com>
2023-08-01 13:31:17 -0300

    feat(register): created Form components for Member and Company register

--- client/app/components/SignUpCompany.jsx
+++ client/app/components/SignUpCompany.jsx
@@ -1,29 +1,64 @@
+import { useState } from "react";
+
 export default function SignUpCompany () {
+    const [input, setInput] = useState({
+        email: '',
+        password: '',
+        companyName: '',
+        description: '',
+        cuit: '',
+        address: '',
+        phoneNumber: '',
+        discount: ''
+    });
+
+    function handleInputChange(e) {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        window.alert("Company submitted successfully (testing)");
+    };
+
     return (
         <div>
-            <form>
-                <label>inputt</label>
-                <input type="" value={false} onChange={false}/>
-                
-        
-                <label>inputt</label>
-                <input type="" value={false} onChange={false}/>
-        
-        
-                <label>inputt</label>
-                <input type="" value={false} onChange={false}/>
-        
-        
-                <label>inputt</label>
-                <input type="" value={false} onChange={false}/>
-        
-        
-                <label>inputt</label>
-                <input type="" value={false} onChange={false}/>
-        
-        
-                <label>inputt</label>
-                <input type="" value={false} onChange={false}/>
+            <form onSubmit={handleSubmit}>
+                <label>Email: </label>
+                <input type="text" name="email" value={input.email} onChange={handleInputChange}/>
+
+
+                <label>Password: </label>
+                <input type="password" name="password" value={input.password} onChange={handleInputChange}/>
+
+
+                <label>Company name: </label>
+                <input type="text" name="companyName" value={input.companyName} onChange={handleInputChange}/>
+
+
+                <label>Description of the company: </label>
+                <input type="text" name="description" value={input.description} onChange={handleInputChange}/>
+
+
+                <label>CUIT: </label>
+                <input type="text" name="cuit" value={input.cuit} onChange={handleInputChange}/>
+
+
+                <label>Address: </label>
+                <input type="text" name="address" value={input.address} onChange={handleInputChange}/>
+
+
+                <label>Phone number: </label>
+                <input type="text" name="phoneNumber" value={input.phoneNumber} onChange={handleInputChange}/>
+
+
+                <label>Discount applied (%): </label>
+                <input type="text" name="discount" value={input.discount} onChange={handleInputChange}/>
+
+                <button>Submit</button>
             </form>
         </div>
     );
--- client/app/components/SignUpMember.jsx
+++ client/app/components/SignUpMember.jsx
@@ -1,28 +1,57 @@
+import { useState } from "react";
+
 export default function SignUpMember () {
+    const [input, setInput] = useState({
+        email: '',
+        password: '',
+        dni: '',
+        firstName: '',
+        lastName: '',
+        address: '',
+        phoneNumber: ''
+    });
+
+    function handleInputChange(e) {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        window.alert("Member submitted successfully (testing)");
+    }
+
     return (
-        <form>
-            <label>inputt</label>
-            <input type="" value={false} onChange={false}/>
+        <form onSubmit={handleSubmit}>
+            <label>Email: </label>
+            <input type="text" name="email" value={input.email} onChange={handleInputChange}/>
             
 
-            <label>inputt</label>
-            <input type="" value={false} onChange={false}/>
+            <label>Password: </label>
+            <input type="password" name="password" value={input.password} onChange={handleInputChange}/>
 
 
-            <label>inputt</label>
-            <input type="" value={false} onChange={false}/>
+            <label>DNI: </label>
+            <input type="text" name="dni" value={input.dni} onChange={handleInputChange}/>
 
 
-            <label>inputt</label>
-            <input type="" value={false} onChange={false}/>
+            <label>First name: </label>
+            <input type="text" name="firstName" value={input.firstName} onChange={handleInputChange}/>
+
+            <label>Last name: </label>
+            <input type="text" name="lastName" value={input.lastName} onChange={handleInputChange}/>
 
 
-            <label>inputt</label>
-            <input type="" value={false} onChange={false}/>
+            <label>Address: </label>
+            <input type="text" name="address" value={input.adress} onChange={handleInputChange}/>
 
 
-            <label>inputt</label>
-            <input type="" value={false} onChange={false}/>
+            <label>Phone number: </label>
+            <input type="text" name="phoneNumber" value={input.phoneNumber} onChange={handleInputChange}/>
+
+            <button>Submit</button>
         </form>
     );
 };
