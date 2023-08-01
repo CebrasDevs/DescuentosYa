de6d98b1075f5dc799f06fa6a129504034e3f838
SnowDevLC <canalesluis9@gmail.com>
2023-08-01 18:38:21 -0300

    fix(bugs/modularizacion): se cambio a carpeta src y arreglo bugs

--- client/src/app/shoppingcart/payment/page.jsx
+++ client/src/app/shoppingcart/payment/page.jsx
@@ -1,7 +1,7 @@
 export default function payment() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500  text-9xl  " >I AM A PAYMENT</h1>
+        <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500  text-9xl  " >I AM A PAYMENT</h1>
       </main>
     )
   }
