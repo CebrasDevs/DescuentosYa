e072541b1135cd95797284d4c54d92878d83b948
Pablo Bestani <pablobestanimouk@gmail.com>
2023-08-03 11:52:27 -0300

    feat(creteItem): created base form rendering and format utils (no logics)

---
+++ client/src/utils/formatUtils.js
@@ -0,0 +1,15 @@
+
+export function formatCompany(company) {
+    //crear logica de formateo de compañia
+    return company;
+};
+
+export function formatMember(member) {
+    //crear logica de formateo de miembro
+    return member;
+};
+
+export function formatItem(item) {
+    //crear logica de formateo de item
+    return item;
+};
