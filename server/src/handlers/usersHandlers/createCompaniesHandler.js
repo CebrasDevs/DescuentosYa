98be1ddaceaae3ac6cc283ede5740b5216c1f5af
Nico Garcia <nicolasgarcia9812@hotmail.com>
2023-08-04 20:34:57 -0300

    Validation of user input data and structuring of response data

--- server/src/handlers/usersHandlers/createCompaniesHandler.js
+++ server/src/handlers/usersHandlers/createCompaniesHandler.js
@@ -1,12 +1,30 @@
-const { createCompaniesController } = require('../../controllers');
+const { createCompaniesController } = require("../../controllers");
 
 module.exports = async (req, res) => {
-    try {
-        let { email, password, cuit, imageUrl, companyName, address, phoneNumber, description } = req.body;
-        let dataCompany = { email, password, cuit, imageUrl, companyName, address, phoneNumber, description };
-        const newCompany = await createCompaniesController(dataCompany);
-        return res.status(200).json(newCompany);
-    } catch (error) {
-        return res.status(400).json({ error: error.message });
+  try {
+    let {
+      email,
+      password,
+      cuit,
+      imageUrl,
+      companyName,
+      address,
+      phoneNumber,
+      description,
+    } = req.body;
+    let dataCompany = {
+      email,
+      password,
+      cuit,
+      imageUrl,
+      companyName,
+      address,
+      phoneNumber,
+      description,
     };
+    const newCompany = await createCompaniesController(dataCompany);
+    return res.status(200).json(newCompany);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 };
