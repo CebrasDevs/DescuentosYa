d0bdd0ee2f607fcf3fb8bd2ba75526a1d2a7c855
Pablo Bestani <pablobestanimouk@gmail.com>
2023-08-04 00:48:23 -0300

    feat(validations): created full validations for company, member and item creation forms

---
+++ client/src/utils/validateCompany.js
@@ -0,0 +1,110 @@
+// algunas regex para validar datos
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const descriptionRegex = /^[0-9a-zA-Z \-\/\+\_\!\¡\¿]+$/;
+const addressRegex = /^[a-zA-Z0-9\s.,'-]+$/;
+const passwordRegex = /^[0-9a-zA-z]+$/;
+const intRegex = /^[0-9]+$/;
+const urlRegex = /^(ftp|http|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/i;
+
+export default function validateCompany(company) {
+    if (!company) {
+        console.log('Invalid company input recieved (at utils/validateCompany)');
+        return {};
+    };
+
+    let errors = {};
+    const {
+    email,
+    password,
+    confirmPassword,
+    companyName,
+    description,
+    cuit,
+    address,
+    phoneNumber,
+    imageUrl
+    } = company;
+
+    // VALIDO EMAIL
+    if (!email.length) {
+        errors.email = 'Required field';
+    } else {
+        if (!emailRegex.test(email)) {
+            errors.email = 'Must be a valid email';
+        };
+    };
+    // VALIDO PASSWORD
+    if (!password.length) {
+        errors.password = 'Required field';
+    } else {
+        if (!passwordRegex.test(password)) {
+            errors.password = 'Must contain only letters andnumbers';
+        };
+        if (password.length < 6 || password.length > 18) {
+            errors.password = 'Must contain between 6 and 18characters';
+        };
+    };
+    // VALIDO CONFIRMPASSWORD
+    if (confirmPassword !== password) {
+        errors.confirmPassword = 'Passwords do not match';
+    };
+    // VALIDO COMPANYNAME
+    if (!companyName.length) {
+        errors.companyName = 'Required field';
+    } else {
+        if (companyName.length > 100) {
+            errors.companyName = 'Company name too long';
+        };
+    };
+    // VALIDO DESCRIPTION
+    if (description.length) {
+        if (!descriptionRegex.test(description)) {
+            errors.description = 'Invalid character';
+        };
+        if (description.length > 250) {
+            errors.description = 'Description must be shorter';
+        };
+    };
+    // VALIDO CUIT
+    if (!cuit.length) {
+        errors.cuit = 'Required field';
+    } else {
+        if (cuit.length < 10 || cuit.length > 11) {
+            errors.cuit = 'Invalid CUIT number';
+        } else if (!intRegex.test(cuit)) {
+            errors.cuit = 'Must contain only numbers';
+        };
+    };
+    // VALIDO ADDRESS
+    if (!address.length) {
+        errors.address = 'Required field';
+    } else {
+        if (!addressRegex.test(address)) {
+            errors.address = 'Invalid address';
+        };
+        if (address.length > 100) {
+            errors.address = 'Address too long';
+        };
+    };
+    // VALIDO PHONENUMBER
+    if (phoneNumber.length) {
+        if (phoneNumber.length < 6) {
+            errors.phoneNumber = 'Phone number too short';
+        };
+        if (phoneNumber.length > 15) {
+            errors.phoneNumber = 'Phone number too long';
+        };
+        if (!intRegex.test(phoneNumber)) {
+            errors.phoneNumber = 'Must contain only numbers';
+        };
+    };
+    // VALIDO IMAGEURL
+    if (!imageUrl.length) {
+        errors.imageUrl = 'Required field';
+    } else {
+        if (!urlRegex.test(imageUrl)) {
+            errors.imageUrl = 'Must be a valid url';
+        };
+    };
+    return errors;
+};
---
+++ client/src/utils/validateItem.js
@@ -0,0 +1,77 @@
+
+// algunas regex para validar datos
+const nameRegex = /^[a-zA-ZáéíóúÁÉÍÓÚàèìòùÀÈÌÒÙâêîôûÂÊÎÔÛ']+$/;
+const descriptionRegex = /^[0-9a-zA-Z \-\/\+\_\!\¡\¿]+$/;
+const floatRegex = /^[0-9.]+$/;
+const intRegex = /^[0-9]+$/;
+const urlRegex = /^(ftp|http|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/i;
+
+
+export default function validateItem(item) {
+    if (!item) {
+        console.log('Invalid item input recieved (at utils/validateItem)');
+        return {};
+    };
+
+    let errors = {};
+    const {
+        name,
+        category,
+        description,
+        price,
+        discount,
+        imageUrl
+    } = item;
+
+
+    // VALIDO NAME
+    if (!name.length) {
+        errors.name = 'Required field';
+    } else {
+        if (!nameRegex.test(name)) {
+            errors.name = 'Must contain only letters, numbers or "-"';
+        };
+        if (name.length > 70) {
+            errors.name = 'Name must be shorter';
+        };
+    };
+    // VALIDO CATEGORY
+    if (category === 'Choose category') {
+        errors.category = 'Please choose a category';
+    };
+    // VALIDO DESCRIPTION
+    if (description.length) {
+        if (!descriptionRegex.test(description)) {
+            errors.description = 'Invalid character';
+        };
+        if (description.length > 250) {
+            errors.description = 'Description must be shorter';
+        };
+    };
+    // VALIDO PRICE
+    if (!price.length) {
+        errors.price = 'Required field';
+    } else {
+        if (!floatRegex.test(price)) {
+            errors.price = 'Must be an integer or a decimal number';
+        };
+    };
+    // VALIDO DISCOUNT
+    if (!discount.length) {
+        errors.discount = 'Required field';
+    } else {
+        if (!intRegex.test(discount)) {
+            errors.discount = 'Must be an integer number';
+        };
+    };
+    // VALIDO IMAGEURL
+    if (!imageUrl.length) {
+        errors.imageUrl = 'Required field';
+    } else {
+        if (!urlRegex.test(imageUrl)) {
+            errors.imageUrl = 'Must be a valid url';
+        };
+    };
+    return errors;
+};
+
