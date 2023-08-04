143cf5e0b13fdc08291a34041098aa76af44839e
Lorenzo Martin del Hoyo <delhoyo.lorenzo@gmail.com>
2023-08-04 11:20:07 -0300

    fix(shoppingcart): bug fixed, cannot add an item twice

--- client/src/components/ItemDetail.jsx
+++ client/src/components/ItemDetail.jsx
@@ -6,10 +6,15 @@ import Link from "next/link";
 export default function ItemDetail({id}){
     const dispatch = useDispatch();
     const allItems = useSelector((state)=> state.allItems)
+    const allShoppingItems = useSelector((state)=> state.allShoppingItems)
     let itemFound = allItems.find((item)=> item.id === Number(id))
 
     const handleAddItem = function(itemFound){
-        dispatch(addShoppinCartItem(itemFound))
+        if(!allShoppingItems.includes(itemFound)){
+            dispatch(addShoppinCartItem(itemFound))
+        }else{
+            alert('Item already added in shopping cart')
+        }
     }
 
     const handleGenerateCode = function(){
@@ -18,22 +23,26 @@ export default function ItemDetail({id}){
 
     if(itemFound.price !== 0){//esto es un servicio
         return(
-            <div>
-                <h1>Imagen del servicio</h1>
-                <h1>Aprovecha el {itemFound.discount} % de descuento en {itemFound.category} en {itemFound.company} </h1>
-                <h1>{itemFound.description}</h1>
-                <Link href={"/shoppingcart"} className="text-blue-700">
-                    <button onClick={()=>handleAddItem(itemFound)}>Agregar al carrito</button>
-                </Link>
+            <div className="bg-violet-200 flex justify-center m-5">
+                <img className="m-5" src={itemFound.img}/>
+                <div className="columns-1 m-5">
+                    <h1>Aprovecha el {itemFound.discount} % de descuento en {itemFound.category} en {itemFound.name} </h1>
+                    <h1>{itemFound.description}</h1>
+                    <Link href={"/shoppingcart"} className="text-blue-700">
+                        <button onClick={()=>handleAddItem(itemFound)}>Agregar al carrito</button>
+                    </Link>
+                </div>
             </div>
         )
     }
     //si la company ofrece productos:
     return(
-        <div>
-            <h1>Imagen del item</h1>
-            <h1>Aprovecha el {itemFound.discount} % de descuento en la seccion {itemFound.category} en {itemFound.company} </h1>
-            <button onClick={handleGenerateCode}>Generar Codigo</button>  
+        <div className="bg-violet-200 flex justify-center m-5">
+            <img className="m-5" src={itemFound.img}/>
+            <div className="columns-1 m-5">
+                <h1>Aprovecha el {itemFound.discount} % de descuento en la seccion {itemFound.category} en {itemFound.name} </h1>
+                <button onClick={handleGenerateCode}>Generar Codigo</button>  
+            </div>
         </div>
     )
 }
