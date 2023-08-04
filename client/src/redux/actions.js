1dc39026feb7b5606fd4fb624905f0e3aee19453
albertog1 <alberto.gentile1@gmail.com>
2023-08-03 21:20:16 -0300

    feat(companyProfile-userProfile): create company and user profile components and redux flow

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -3,6 +3,8 @@ export const FILTER_CARDS = "FILTER_CARDS";
 export const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
 export const ADD_SHOPPING_CART_ITEM = "ADD_SHOPPING_CART_ITEM";
 export const DELETE_SHOPPING_CART_ITEM = "DELETE_SHOPPING_CART_ITEM";
+export const DELETE_COMPANY_ITEM = 'DELETE_COMPANY_ITEM';
+
 
 
 
@@ -24,4 +26,8 @@ export const addShoppinCartItem = (item) => {
 
 export const deleteShoppingCartItem = (id) => {
     return {type: DELETE_SHOPPING_CART_ITEM, payload: id};
-}
+};
+
+export const deleteCompanyItem = (id) => {
+    return {type: DELETE_COMPANY_ITEM, payload: id};
+};
