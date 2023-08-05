a24d78c52dc133081e63129cad814d087321e917
Pablo Bestani <pablobestanimouk@gmail.com>
2023-08-05 18:34:53 -0300

    fix(controllers/itemsControllers): added item id to controller for fetching

--- server/src/controllers/itemsControllers/getItemsController.js
+++ server/src/controllers/itemsControllers/getItemsController.js
@@ -5,6 +5,7 @@ module.exports = async () => {
 
     const itemsInfo = result.map((item) => {
         return {
+            id: item.id,
             url_image: item.url_image,
             name: item.name,
             company: item.user.company_name,
