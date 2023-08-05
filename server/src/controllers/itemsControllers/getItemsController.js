37893a53af1c4a88c4c31f9afb30cdbc81c50620
wtfranco22 <francofranco_96@hotmail.com>
2023-08-05 20:12:57 -0300

    fix(getItemsController): The ID property is added so that they can perform an item detail in the future

--- server/src/controllers/itemsControllers/getItemsController.js
+++ server/src/controllers/itemsControllers/getItemsController.js
@@ -5,6 +5,7 @@ module.exports = async () => {
 
     const itemsInfo = result.map((item) => {
         return {
+            id: item.id,
             imageUrl: item.url_image,
             name: item.name,
             company: item.user.company_name,
