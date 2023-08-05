ffdcb46c56deb0219286d13709635e68da7be372
wtfranco22 <francofranco_96@hotmail.com>
2023-08-05 03:34:36 -0300

    fix(config): ports were configured, cors access and seeder was fixed

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ app.use(morgan('dev'));
 
 // Establecemos cabeceras de acceso CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
