fbdc7301c02cebf7f34331c83d01b6e68a5fed11
Lorenzo Martin del Hoyo <delhoyo.lorenzo@gmail.com>
2023-08-06 11:18:36 -0300

    feat(CompanyDetail): company detail added, linked in brands section and carousel

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -7,58 +7,72 @@ export const FILTER_CARDS = "FILTER_CARDS";
 export const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
 export const ADD_SHOPPING_CART_ITEM = "ADD_SHOPPING_CART_ITEM";
 export const DELETE_SHOPPING_CART_ITEM = "DELETE_SHOPPING_CART_ITEM";
-export const DELETE_COMPANY_ITEM = 'DELETE_COMPANY_ITEM';
-
-
+export const DELETE_COMPANY_ITEM = "DELETE_COMPANY_ITEM";
+export const GET_COMPANY_DETAIL = "GET_COMPANY_DETAIL";
+export const CLEAN_COMPANY_DETAIL = "CLEAN_COMPANY_DETAIL";
 
 
 export const getCompanies = () => {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios.get("http://localhost:3001/companies");
-            return dispatch({
-                type: GET_COMPANIES,
-                payload: data,
-            });
-        } catch (error) {
-            console.log("error");
-        }
-        
-    };
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get("http://localhost:3001/companies");
+      return dispatch({
+        type: GET_COMPANIES,
+        payload: data,
+      });
+    } catch (error) {
+      console.log("error");
+    }
+  };
 };
 
 export const getDiscounts = () => {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios.get("http://localhost:3001/items");
-            return dispatch({
-                type: GET_DISCOUNTS,
-                payload: data,
-            });
-        } catch (error) {
-            console.log("error");
-        }
-        
-    };
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get("http://localhost:3001/items");
+      return dispatch({
+        type: GET_DISCOUNTS,
+        payload: data,
+      });
+    } catch (error) {
+      console.log("error");
+    }
+  };
 };
 
 export const getCategories = () => {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios.get("http://localhost:3001/categories");
-            return dispatch({
-                type: GET_CATEGORIES,
-                payload: data,
-            });
-        } catch (error) {
-            console.log("error");
-        }
-        
-    };
+  return async (dispatch) => {
+      try {
+          const { data } = await axios.get("http://localhost:3001/categories");
+      return dispatch({
+        type: GET_CATEGORIES,
+        payload: data,
+      });
+    } catch (error) {
+        console.log("error");
+    }
+};
 };
 
+export const getCompanyDetail = (id) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`http://localhost:3001/companies/${id}`);
+      return dispatch({
+        type: GET_COMPANY_DETAIL,
+        payload: data[0],
+      });
+    } catch (error) {
+      console.log("error");
+    }
+  };
+};
+
+export const cleanCompanyDetail = () => {
+  return { type: CLEAN_COMPANY_DETAIL };
+};
 export const filterCards = (filters) => {
-    return {type: FILTER_CARDS, payload: filters};
+  return { type: FILTER_CARDS, payload: filters };
 };
 
 export const setCurrentPage = (page) => {
@@ -66,13 +80,13 @@ export const setCurrentPage = (page) => {
 };
 
 export const addShoppinCartItem = (item) => {
-    return {type: ADD_SHOPPING_CART_ITEM, payload: item};
+    return { type: ADD_SHOPPING_CART_ITEM, payload: item };
 };
 
 export const deleteShoppingCartItem = (id) => {
-    return {type: DELETE_SHOPPING_CART_ITEM, payload: id};
+  return { type: DELETE_SHOPPING_CART_ITEM, payload: id };
 };
 
 export const deleteCompanyItem = (id) => {
-    return {type: DELETE_COMPANY_ITEM, payload: id};
+  return { type: DELETE_COMPANY_ITEM, payload: id };
 };
