a7b5a948b622b6cb9256b6c7ae87aa2a6335fac9
misaelc <misaelclemente98@gmail.com>
2023-08-06 11:17:27 -0300

    style(/client):minor fixes

--- client/src/app/discounts/page.jsx
+++ client/src/app/discounts/page.jsx
@@ -1,14 +1,16 @@
 import Filters from "@/components/Filters";
 import Grid from "@/components/Grid";
+import Pagination from "@/components/Pagination";
 
 export default function discounts() {
     return (
-        <div className="grid grid-cols-4 gap-4 w-4/5 m-auto my-10">
+        <div className=" grid grid-cols-4 gap-4 w-4/5 m-auto my-10">
             <div className="col-span-4 bg-violet-200 h-10">Search bar</div>
-            <div className=" bg-gradient-to-b from-violet-200 to-white rounded-lg">
+            <div className=" bg-gradient-to-b from-violet-200 to-slate-200 rounded-lg ">
                 <Filters />
+                <Pagination/>
             </div>
-            <div className="col-span-3 rounded-lg">
+            <div className="col-span-3 rounded-lg ">
                 <Grid />
             </div>
         </div>
