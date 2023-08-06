517d1b2ccd4a2d602c00d3ec60d8a4cb2a645bf9
Alberto Mallar <129788363+AlbertoMallar@users.noreply.github.com>
2023-08-06 13:24:36 -0300

    fix(updateItemHelper): Object created with express operator instead of using a condition for each property
    Colocada la mejora que Luis propuso

--- server/src/helpers/itemsHelpers/updateItemsHelper.js
+++ server/src/helpers/itemsHelpers/updateItemsHelper.js
@@ -7,15 +7,15 @@ module.exports = async (id, dataItem) => {
     console.log(id)
     console.log(dataItem)
 
-    const updateData = {}
-
-    if (dataItem.url_image) updateData.url_image = dataItem.url_image;
-    if (dataItem.name) updateData.name = dataItem.name;
-    if (dataItem.description) updateData.description = dataItem.description;    
-    if (dataItem.categoryId) updateData.categoryId = dataItem.categoryId;
-    if (dataItem.price) updateData.price = dataItem.price;
-    if (dataItem.discount) updateData.discount = dataItem.discount;
-    if (dataItem.id === null) updateData.id = +id;
+    const updateData = {
+        ...dataItem,
+        id: dataItem.id === null ? +id : dataItem.id,
+        };
+        
+        const res = await prisma.item.update({
+            where: { id: +id },
+            data: updateData,
+        });
 
     
     const res = await prisma.item.update({
