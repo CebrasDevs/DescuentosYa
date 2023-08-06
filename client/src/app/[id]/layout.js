557441c40a2de948c6062b77e8d4841769fe8966
misaelc <misaelclemente98@gmail.com>
2023-08-05 23:38:30 -0300

    feat(/ItemDetail): Details showed correctly,style(/pagination,/global.css):styled pagination and global background

--- client/src/app/[id]/layout.js
+++ client/src/app/[id]/layout.js
@@ -1,10 +1,13 @@
 import ItemDetail from "@/components/ItemDetail";
+import axios from "axios";
 
-export default function Layout({params}){
-    const { id } = params;
-    return(
-        <div>
-            <ItemDetail id={id}/>
-        </div>
-    )
+export default async function Layout({ params }) {
+  const { id } = params;
+  const { data } = await axios(`http://localhost:3001/items/${id}`);
+
+  return (
+    <div>
+      <ItemDetail data={data} />
+    </div>
+  );
 }
--- client/src/components/ItemDetail.jsx
+++ client/src/components/ItemDetail.jsx
@@ -1,48 +1,59 @@
-'use client'
-import { useSelector, useDispatch } from "react-redux"
+"use client";
+import { useDispatch, useSelector } from "react-redux";
 import { addShoppinCartItem } from "@/redux/actions";
 import Link from "next/link";
 
-export default function ItemDetail({id}){
-    const dispatch = useDispatch();
-    const allItems = useSelector((state)=> state.allItems)
-    const allShoppingItems = useSelector((state)=> state.allShoppingItems)
-    let itemFound = allItems.find((item)=> item.id === Number(id))
+export default function ItemDetail({ data }) {
+    console.log(data);
+  const dispatch = useDispatch();
+  const allShoppingItems = useSelector((state)=> state.allShoppingItems)
+  
 
-    const handleAddItem = function(itemFound){
-        if(!allShoppingItems.includes(itemFound)){
-            dispatch(addShoppinCartItem(itemFound))
-        }else{
-            alert('Item already added in shopping cart')
-        }
+  const handleAddItem = function (itemFound) {
+    if (!allShoppingItems.includes(itemFound)) {
+      dispatch(addShoppinCartItem(itemFound));
+    } else {
+      alert("Item already added in shopping cart");
     }
+  };
 
-    const handleGenerateCode = function(){
-        //logica de generacion de codigo, charlar
-    }
+  const handleGenerateCode = function () {
+    //logica de generacion de codigo, charlar
+  };
 
-    if(itemFound.price !== 0){//esto es un servicio
-        return(
-            <div className="bg-violet-200 flex justify-center m-5">
-                <img className="m-5" src={itemFound.img}/>
-                <div className="columns-1 m-5">
-                    <h1>Aprovecha el {itemFound.discount} % de descuento en {itemFound.category} en {itemFound.name} </h1>
-                    <h1>{itemFound.description}</h1>
-                    <Link href={"/shoppingcart"} className="text-blue-700">
-                        <button onClick={()=>handleAddItem(itemFound)}>Agregar al carrito</button>
-                    </Link>
-                </div>
-            </div>
-        )
-    }
-    //si la company ofrece productos:
-    return(
-        <div className="bg-violet-200 flex justify-center m-5">
-            <img className="m-5" src={itemFound.img}/>
-            <div className="columns-1 m-5">
-                <h1>Aprovecha el {itemFound.discount} % de descuento en la seccion {itemFound.category} en {itemFound.name} </h1>
-                <button onClick={handleGenerateCode}>Generar Codigo</button>  
-            </div>
+  if (data.price !== 0) {
+    //esto es un servicio
+    return (
+      <div className="bg-violet-200 flex justify-center m-5">
+        <img className="m-5" src={data.url_image} />
+        <div className="columns-1 m-5">
+          <h1>
+            Aprovecha el {data.discount} % de descuento en {data.category}{" "}
+            en {data.name}{" "}
+          </h1>
+          <h1>{data.description}</h1>
+          <h1>${data.price}</h1>
+          <Link href={"/shoppingcart"} className="text-blue-700">
+            <button onClick={() => handleAddItem(data)}>
+              Agregar al carrito
+            </button>
+          </Link>
         </div>
-    )
+      </div>
+    );
+  }
+  
+  //si la company ofrece productos:
+  return (
+    <div className="bg-violet-200 flex justify-center m-5">
+      <img className="m-5" src={data.url_image} />
+      <div className="columns-1 m-5">
+        <h1>
+          Aprovecha el {data.discount} % de descuento en la seccion{" "}
+          {data.category} en {data.name}{" "}
+        </h1>
+        <button onClick={handleGenerateCode}>Generar Codigo</button>
+      </div>
+    </div>
+  );
 }
