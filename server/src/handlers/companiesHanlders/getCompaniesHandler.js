d182309c51bf1ad760d6851d215944ccf9bf3ab0
Luis Canales <129117019+SnowDevLC@users.noreply.github.com>
2023-08-07 05:22:17 -0300

    Update getCompaniesHandler.js

--- server/src/handlers/companiesHanlders/getCompaniesHandler.js
+++ server/src/handlers/companiesHanlders/getCompaniesHandler.js
@@ -2,7 +2,6 @@ const { getCompaniesController } = require('../../controllers');
 
 module.exports = async (req, res) => {
     try {
-        console.log("conexion deploy funciona");
         const companies = await getCompaniesController();
         return res.status(200).json(companies);
     } catch (error) {
