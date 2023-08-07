b7c5dbe50bce72754953925372040ba0b99667d2
Luis Canales <129117019+SnowDevLC@users.noreply.github.com>
2023-08-07 11:07:47 -0300

    Update layout.js

--- client/src/app/[id]/layout.js
+++ client/src/app/[id]/layout.js
@@ -1,13 +1,10 @@
-import ItemDetail from "@/components/ItemDetail";
-import axios from "axios";
 
-export default async function Layout({ params }) {
-  const { id } = params;
-  const { data } = await axios(`https://desceuntosya-back.onrender.com/items/${id}`);
+export default async function Layout({ children }) {
+
 
   return (
     <div>
-      <ItemDetail data={data} />
+      {children}
     </div>
   );
 }
