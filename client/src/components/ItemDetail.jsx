e62a102eefef5bd7ead7d437b5ab0925890befee
Luis Canales <129117019+SnowDevLC@users.noreply.github.com>
2023-08-08 20:31:26 -0300

    Update ItemDetail.jsx

--- client/src/components/ItemDetail.jsx
+++ client/src/components/ItemDetail.jsx
@@ -39,7 +39,7 @@ export default function ItemDetail({ data }) {
             <div className=" w-1/2 h-full">
               <img
                 className="w-10/12 rounded-2xl mt-6 mb-6 ml-10 mr-6 border-2 border-gray-300"
-                src={data.url_image}
+                src={data.imageUrl}
               ></img>
             </div>
             <ModifiedItem data={data} type={"service"} />
@@ -99,7 +99,7 @@ export default function ItemDetail({ data }) {
           <div className=" w-1/2 h-full">
             <img
               className="w-10/12 rounded-2xl mt-6 mb-6 ml-10 mr-6 border-2 border-gray-300"
-              src={data.url_image}
+              src={data.imageUrl}
             ></img>
           </div>
           <ModifiedItem data={data} type={"product"} />
