435bf28f221dcb6faf6aedd1e7f6bcafa614a37a
wtfranco22 <francofranco_96@hotmail.com>
2023-08-09 01:23:13 -0300

    fix(Models): enabled options and consequently modifications in CRUD roles

---
+++ server/src/controllers/companiesControllers/updateCompaniesController.js
@@ -0,0 +1,41 @@
+const { updateUsersHelper } = require("../../helpers");
+const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const regexName = /^[a-zA-ZÀ-ÿ\s'-]*$/;
+module.exports = async (dataCompany) => {
+  //si la propiedad existe, debe cumplir con su tipo de dato en caso que no corresponda ingresa a error
+  if (
+    isNaN(dataCompany.id) ||
+    (dataCompany.email && !regexEmail.test(dataCompany.email)) ||
+    (dataCompany.password && !isNaN(dataCompany.password)) ||
+    dataCompany.enabled ||
+    (dataCompany.cuit && isNaN(dataCompany.dni_cuit)) ||
+    (dataCompany.name && regexName.test(dataCompany.name)) ||
+    (dataCompany.address && !isNaN(dataCompany.address))
+  )
+    throw new Error("Incomplete data or incorrect");
+  let { id } = dataCompany;
+  const updatedCompany = await updateUsersHelper(id, dataCompany);
+  let {
+    email,
+    enabled,
+    role,
+    dni_cuit,
+    name,
+    urlImage,
+    address,
+    phoneNumber,
+    description
+  } = updatedCompany;
+  return {
+    id,
+    email,
+    enabled,
+    role,
+    cuit: dni_cuit,
+    name,
+    urlImage,
+    address,
+    phoneNumber,
+    description
+  };
+};
