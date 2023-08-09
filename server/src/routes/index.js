ad6537b69193aa49b3d508daa406748beed4e035
Nico Garcia <nicolasgarcia9812@hotmail.com>
2023-08-09 16:38:50 -0300

    feat(routes): Authentication JWT Back

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,27 @@
 const { Router } = require("express");
 const mainRouter = Router();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+// Middleware para verificar el token en rutas protegidas
+function verificarToken(req, res, next) {
+  const { id } = req.query;
+  const token = req.cookies[id];
+  // console.log(token);
+  // console.log(req)
+
+  if (!token) {
+    return res.status(401).json({ mensaje: "Token not provided" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      return res.status(401).json({ mensaje: "Invalid or expired token" });
+    }
+    req.usuarioId = decodedToken.userId;
+    next();
+  });
+}
 
 const shoppingRouter = require("./shoppingRouter");
 const itemsRouter = require("./itemsRouter");
@@ -9,14 +31,18 @@ const companiesRouter = require("./companiesRouter");
 const membersRouter = require("./membersRouter");
 const vouchersRouter = require("./vouchersRouter");
 const categoriesRouter = require("./categoriesRouter");
+const authRouter = require("./authRouter");
+const authRouterCookie = require("./authRouterCookie");
 
-mainRouter.use("/shopping", shoppingRouter);
+// Usamos el middleware para verificar el token en rutas protegidas
+mainRouter.use("/shopping", verificarToken, shoppingRouter);
 mainRouter.use("/items", itemsRouter);
-mainRouter.use("/users", usersRouter);
-mainRouter.use("/admins", adminsRouter);
+mainRouter.use("/users", verificarToken, usersRouter);
+mainRouter.use("/admins", verificarToken, adminsRouter);
 mainRouter.use("/companies", companiesRouter);
 mainRouter.use("/members", membersRouter);
-mainRouter.use("/vouchers", vouchersRouter);
+mainRouter.use("/vouchers", verificarToken, vouchersRouter);
 mainRouter.use("/categories", categoriesRouter);
+mainRouter.use("/", authRouter);
 
 module.exports = mainRouter;
