b1286eb1475b1322504ecf9a1bd7d2256d6f2556
Nico Garcia <nicolasgarcia9812@hotmail.com>
2023-08-10 15:49:41 -0300

    fix(routes): Authentication

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,26 +1,43 @@
+const express = require("express");
 const { Router } = require("express");
 const mainRouter = Router();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 // Middleware para verificar el token en rutas protegidas
-function verificarToken(req, res, next) {
-  const { id } = req.query;
-  const token = req.cookies[id];
-  // console.log(token);
-  // console.log(req)
-
-  if (!token) {
-    return res.status(401).json({ mensaje: "Token not provided" });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-    if (err) {
-      return res.status(401).json({ mensaje: "Invalid or expired token" });
+async function verificarToken(req, res, next) {
+  try {
+    if(!req.cookies.accessTrue){
+      return res.status(401).json({ message: "Token not provided" });
     }
-    req.usuarioId = decodedToken.userId;
-    next();
-  });
+
+    const token = req.cookies.accessTrue.token;
+
+    if (!token) {
+      return res.status(401).json({ message: "Token not provided" });
+    }
+
+    const id = req.cookies.accessTrue.user.id;
+console.log(req.cookies)
+    const user = await prisma.user.findUnique({
+      where: { id }, // Buscar al usuario por su ID
+    });
+
+
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err || user.id !== decodedToken.userId) {
+        return res.status(401).json({ message: "Invalid or expired token" });
+      }
+      req.userId = decodedToken.userId;
+      next();
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 const shoppingRouter = require("./shoppingRouter");
@@ -42,7 +59,7 @@ mainRouter.use("/shopping", verificarToken, shoppingRouter);
 mainRouter.use("/items", itemsRouter);
 mainRouter.use("/users", verificarToken, usersRouter);
 mainRouter.use("/admins", verificarToken, adminsRouter);
-mainRouter.use("/companies", companiesRouter);
+mainRouter.use("/companies", verificarToken, companiesRouter);
 mainRouter.use("/members", membersRouter);
 mainRouter.use("/vouchers", verificarToken, vouchersRouter);
 mainRouter.use("/categories", categoriesRouter);
