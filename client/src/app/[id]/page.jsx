26370525965cd6099f61eee12ae269ee898c31a6
Luis Canales <129117019+SnowDevLC@users.noreply.github.com>
2023-08-11 12:58:05 -0300

    Update page.jsx

--- client/src/app/[id]/page.jsx
+++ client/src/app/[id]/page.jsx
@@ -1,10 +1,11 @@
 "use client"
 import ItemDetail from "@/components/ItemDetail";
 import axios from "axios";
+import { URL_BASE } from "@/utils/const.js";
 
 export default async function detail( { params } ) {
     const { id } = params;
-    const { data } = await axios(`https://desceuntosya-back.onrender.com/items/${id}`);
+    const { data } = await axios(`${URL_BASE}/${id}`);
 
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
