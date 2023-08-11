449fa938e25d95f0f70bad31a57cbc6f6898f5d2
Luis Canales <129117019+SnowDevLC@users.noreply.github.com>
2023-08-11 13:01:26 -0300

    Update page.jsx

--- client/src/app/[id]/page.jsx
+++ client/src/app/[id]/page.jsx
@@ -5,7 +5,7 @@ import { URL_BASE } from "@/utils/const.js";
 
 export default async function detail( { params } ) {
     const { id } = params;
-    const { data } = await axios(`${URL_BASE}/${id}`);
+    const { data } = await axios(`${URL_BASE}/items/${id}`);
 
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
