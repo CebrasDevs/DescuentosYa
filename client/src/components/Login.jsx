1e56ae95937ab5b5f99951620da02b722b5f049f
Luis Canales <129117019+SnowDevLC@users.noreply.github.com>
2023-08-11 12:29:51 -0300

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -31,15 +31,15 @@ export default function Login() {
                 // {withCredentials: true}
             );
             if (response.status === 200) {
-                //localStorage.setItem("token", response.data.data.token)
+                localStorage.setItem("token", response.data.data.token)
                 localStorage.setItem("id", JSON.stringify(response.data.data.id))
-                //localStorage.setItem("role", JSON.stringify(response.data.data.role))
+                localStorage.setItem("role", JSON.stringify(response.data.data.role))
                 window.alert(response.data.data.message);
                 setInput({
                     name: "",
                     password: "",
                 });
-                //window.location.href = "https://descuentos-ya.vercel.app/";
+                window.location.href = "https://descuentos-ya.vercel.app/";
             }
         } catch (error) {
             window.alert(error.response.data.error);
