2013a20aee9c5df1067c75b9a47d4c0c08c70cd9
Luis Canales <129117019+SnowDevLC@users.noreply.github.com>
2023-08-11 15:26:59 -0300

    Update getProfileController.js

--- server/src/controllers/usersControllers/getProfileController.js
+++ server/src/controllers/usersControllers/getProfileController.js
@@ -16,27 +16,28 @@ module.exports = async (id) => {
         return structureMember(user);
 
     } else if (user.role === 'COMPANY') {
-        const company = structureCompany(user);
-        const itemIds = company.items.map(({ id }) => id);
-        let shoppingIds = [];
+        return structureCompany(user);
+        //const company = structureCompany(user);
+        //const itemIds = company.items.map(({ id }) => id);
+        //let shoppingIds = [];
 
-        user.Item.map((item) => {
-            item.Item_Shopping.map((record) => {
-                shoppingIds.push(record.shoppingId);
-            });
-        });
+        //user.Item.map((item) => {
+        //    item.Item_Shopping.map((record) => {
+        //        shoppingIds.push(record.shoppingId);
+        //    });
+        //});
 
-        let vouchers = await getVouchersHelper({itemId: {in: itemIds}});
-        vouchers = structureVouchers(vouchers);
+        //let vouchers = await getVouchersHelper({itemId: {in: itemIds}});
+        //vouchers = structureVouchers(vouchers);
 
-        let shoppings = await getShoppingHelper({id: {in: shoppingIds}});
-        shoppings = structureShoppings(shoppings);
+        //let shoppings = await getShoppingHelper({id: {in: shoppingIds}});
+        //shoppings = structureShoppings(shoppings);
         
-        return {
-            ...company,
-            sales: shoppings,
-            vouchers,
-        };
+        //return {
+        //    ...company,
+        //    sales: shoppings,
+        //    vouchers,
+        //};
         
     } else if (user.role === 'ADMIN') {
         return structureAdmin(user);
