d32b86689753edfcb9e589b29f36303e2503b201
Luis Canales <129117019+SnowDevLC@users.noreply.github.com>
2023-08-11 12:55:54 -0300

    Update layout.js

--- client/src/app/[id]/layout.js
+++ client/src/app/[id]/layout.js
@@ -1,10 +1,4 @@
-import ItemDetail from "@/components/ItemDetail";
-import { URL_BASE } from "@/utils/const";
-import axios from "axios";
-
 export default async function Layout({ params }) {
-  const { id } = params;
-  const { data } = await axios(`${URL_BASE}/items/${id}`);
   
   return (
     <div>
