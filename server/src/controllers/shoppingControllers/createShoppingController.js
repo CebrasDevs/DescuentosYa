60231ef2c1dc0019b8ef4b5d9b5bc1c561e39c51
Pablo Bestani <pablobestanimouk@gmail.com>
2023-08-11 21:24:49 -0300

    feat(createShoppings): created controller logics for generating shopping records

--- server/src/controllers/shoppingControllers/createShoppingController.js
+++ server/src/controllers/shoppingControllers/createShoppingController.js
@@ -1,6 +1,46 @@
-const {createShoppingHelper} = require('../../helpers');
+const {createShoppingHelper, createItemShoppingHelper} = require('../../helpers');
 
-module.exports = async (shopping) => {
-    const result = await createShoppingHelper(shopping);
-    return (result)
+module.exports = async (data) => {
+    // Modelo input ideal
+    // const data = {
+    //     userId,
+    //     wayToPay,
+    //     state,
+    //     totalPrice,
+    //     items: [{
+    //         id,
+    //         quantity
+    //     }],
+    // }
+    const {
+        userId,
+        wayToPay,
+        state,
+        totalPrice,
+        items
+    } = data;
+
+    // Reemplazar por la url del PDF generado
+    const pdfUrl = '';
+    const shopping = {
+        userId,
+        pdfUrl,
+        wayToPay,
+        state,
+        totalPrice
+    };
+    const shoppingResponse = await createShoppingHelper(shopping);
+
+    const ItemShoppingPromises = items.map((item) => {
+        return createItemShoppingHelper({
+            itemId: item.id,
+            shoppingId: shoppingResponse.id,
+            quantityItem: item.quantity
+        });
+    });
+
+    return {
+        shopping: shoppingResponse,
+        Item_Shopping: await Promise.all(ItemShoppingPromises)
+    };
 };
