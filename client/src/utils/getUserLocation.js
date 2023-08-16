64f0f8495c041452ad61e8bcd316a3b062873edf
Pablo Bestani <pablobestanimouk@gmail.com>
2023-08-15 22:59:04 -0300

    feat(geolocation): created full logics for sorting items based on distance from user

--- client/src/utils/getUserLocation.js
+++ client/src/utils/getUserLocation.js
@@ -1,5 +1,6 @@
 
 export default async function getUserLocation() {
+    // Consigue la ubicacion actual del navegador del usuario
     return new Promise((resolve, reject) => {
 
         navigator.geolocation.getCurrentPosition(
@@ -12,5 +13,11 @@ export default async function getUserLocation() {
                 reject();
             }
         )
+    })
+    .then((coords) => {
+        return {
+            lat: coords[1],
+            lng: coords[0]
+        }
     });
 };
