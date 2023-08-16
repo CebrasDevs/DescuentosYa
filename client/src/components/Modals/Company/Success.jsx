42938dbebb1bad419d5ecd1cdd441bbdd7379b33
misaelc <misaelclemente98@gmail.com>
2023-08-16 20:31:52 -0300

    cambios menores

--- client/src/components/Modals/Company/Success.jsx
+++ client/src/components/Modals/Company/Success.jsx
@@ -11,29 +11,29 @@ export default function SuccessModal() {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             stroke="#338e39"
-            stroke-width="4.8"
+            strokeWidth="4.8"
           >
             {" "}
             <path
               d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
               stroke="#11d80e"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></path>{" "}
             <path
               opacity="0.34"
               d="M7.75 11.9999L10.58 14.8299L16.25 9.16992"
               stroke="#11d80e"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></path>{" "}
           </g>
           <g id="SVGRepo_iconCarrier">
@@ -41,25 +41,25 @@ export default function SuccessModal() {
             <path
               d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
               stroke="#11d80e"
-              stroke-width="2.4"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2.4"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></path>{" "}
             <path
               opacity="0.34"
               d="M7.75 11.9999L10.58 14.8299L16.25 9.16992"
               stroke="#11d80e"
-              stroke-width="2.4"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2.4"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></path>{" "}
           </g>
         </svg>
-        <p class="mt-4 text-center text-xl font-bold">Success</p>
-        <p class="mt-2 text-center text-lg">
+        <p className="mt-4 text-center text-xl font-bold">Success</p>
+        <p className="mt-2 text-center text-lg">
           Your account was created successfully
         </p>
-        <div class="mt-8 flex flex-col justify-center space-y-3 sm:flex-row sm:space-x-3 sm:space-y-0">
+        <div className="mt-8 flex flex-col justify-center space-y-3 sm:flex-row sm:space-x-3 sm:space-y-0">
           <Link href="/">
             <button className="whitespace-nowrap rounded-md bg-green-600 px-4 py-3 font-medium text-white">
               Continue
