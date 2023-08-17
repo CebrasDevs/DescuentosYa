e85e1ebd5ff0de23305652c95b23fc7e20d67f3c
Pablo Bestani <pablobestanimouk@gmail.com>
2023-08-17 18:21:19 -0300

    fix(RootConfig): added useful comments

--- client/src/components/RootConfig.jsx
+++ client/src/components/RootConfig.jsx
@@ -7,23 +7,29 @@ import { useLoadScript } from "@react-google-maps/api";
 
 export default function RootConfig() {
   const dispatch = useDispatch();
+  // Cargamos el script de GoogleMaps para poder usar los mapas en toda la pagina
   useLoadScript({ googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY });
+
   useEffect(() => {
+    // Hidratamos los estados globales
     dispatch(getCompanies());
     dispatch(getDiscounts());
     dispatch(getCategories());
-    // Acceder a una cookie
+
+    // Conseguimos el usuario activo a partir de la cookie
     const retrievedCookie = Cookies.get("accessTrue");
     if (retrievedCookie) {
         const parsedValue = JSON.parse(retrievedCookie);
+        // Lo guardamos en el estado global
         dispatch(setActiveUser(parsedValue.id));
     }
+    // Conseguimos los items que haya en las cookies
     const cart = Cookies.get("shoppingCart");
     if (cart) {
       const cartValue = JSON.parse(cart);
+      // Los guardamos en el estado global
       dispatch(setShoppingCart(cartValue));
     }
-    // agregar el loadscript de gmaps
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
