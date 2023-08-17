16c37a557f6fa8e780649a43e6e65f88350fc3ba
Pablo Bestani <pablobestanimouk@gmail.com>
2023-08-17 03:07:10 -0300

    feat(carousel): attempted custom items carousel logics

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,9 +1,10 @@
 "use client"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { useSelector } from "react-redux";
 import Link from "next/link";
 import Card from "./Card";
+// import sortItemsCarousel from "@/utils/geolocationUtils/sortItemsCarousel";
 
 export default function Carousel({ value }) {
   const itemsPerPage = 3;
@@ -12,6 +13,14 @@ export default function Carousel({ value }) {
   const companies = useSelector((state) => state.companies);
 
   const [currentPage, setCurrentPage] = useState(0);
+
+  // useEffect(() => {
+  //   const userLocation = JSON.parse(localStorage.getItem("userLocation"));
+  //   if (userLocation) {
+  //     companies.sort((a, b) => a.distance.value - b.distance.value);
+  //     items = sortItemsCarousel(items);
+  //   };
+  // }, []);
 
   const data = value === "discounts" ? items : companies;
   const totalPages = Math.ceil(data.length / itemsPerPage);
