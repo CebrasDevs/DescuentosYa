72b7453814d7f7dfbc3680eaa262fe76dc6d8ae0
Pablo Bestani <pablobestanimouk@gmail.com>
2023-08-17 23:39:53 -0300

    feat(RootConfig): created full RootConfig logics

--- client/src/components/RootConfig.jsx
+++ client/src/components/RootConfig.jsx
@@ -1,14 +1,17 @@
 'use client'
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getCategories, getCompanies, getDiscounts, setActiveUser, setShoppingCart } from "@/redux/actions";
-import Cookies from "js-cookie";
+import { useDispatch, useSelector } from "react-redux";
 import { useLoadScript } from "@react-google-maps/api";
+import Cookies from "js-cookie";
+import { getCategories, getCompanies, getDiscounts, setActiveUser, setDistances, setShoppingCart } from "@/redux/actions";
+
 
 export default function RootConfig() {
   const dispatch = useDispatch();
   // Cargamos el script de GoogleMaps para poder usar los mapas en toda la pagina
-  useLoadScript({ googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY });
+  const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY });
+  const companies = useSelector((state) => state.companies);
+
 
   useEffect(() => {
     // Hidratamos los estados globales
@@ -22,16 +25,38 @@ export default function RootConfig() {
         const parsedValue = JSON.parse(retrievedCookie);
         // Lo guardamos en el estado global
         dispatch(setActiveUser(parsedValue.id));
-    }
+    };
+
     // Conseguimos los items que haya en las cookies
     const cart = Cookies.get("shoppingCart");
     if (cart) {
       const cartValue = JSON.parse(cart);
       // Los guardamos en el estado global
       dispatch(setShoppingCart(cartValue));
-    }
+    };
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+
+
+
+  useEffect(() => {
+    // Necesitamos que las compañias ya esten en el estado global,
+    //  que el script de GoogleMaps este listo,
+    //  y que aun no se hayan cargado las distancias antes;
+
+    if (companies.length && isLoaded && !("distance" in companies[0])) {
+        // Chequeamos si ya hay userLocation guardada en el localStorage
+        const userLocation = JSON.parse(localStorage.getItem("userLocation"));
+        if (userLocation) {
+            dispatch(setDistances(companies));
+        };
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [companies, isLoaded])
+
+
+    // Como esto es un componente que no renderiza nada, retornamos null
     return null;
 }
