dec6f69d5995228bb32f2fdc233eef3d42e21295
misaelc <misaelclemente98@gmail.com>
2023-08-21 19:23:51 -0300

    Style(/CompanyProfile):Styles

--- client/src/components/CompanyDetail.jsx
+++ client/src/components/CompanyDetail.jsx
@@ -5,41 +5,88 @@ import Grid from "./Grid";
 import Map from "./Map";
 
 export default function CompanyDetail({ id }) {
-    const company = useCompany(id);
+  const company = useCompany(id);
 
-    function formatedCuit(cuit){
-        if (cuit.length !== 11) {
-            return "El CUIT debe tener 11 caracteres";
-        }
-        const cuitFormated = `${cuit.substr(0, 2)}-${cuit.substr(2, 8)}-${cuit.substr(10, 1)}`;
-        return cuitFormated;
+  function formatedCuit(cuit) {
+    if (cuit.length !== 11) {
+      return "El CUIT debe tener 11 caracteres";
     }
+    const cuitFormated = `${cuit.substr(0, 2)}-${cuit.substr(
+      2,
+      8
+    )}-${cuit.substr(10, 1)}`;
+    return cuitFormated;
+  }
 
-    if (!company.id) {
-        return <p>Loading...</p>;
-    }
-    return (
-        <div className="mt-10 pb-10 bg-slate-100 rounded-lg shadow-md w-3/4 mx-auto">
-            <div className="flex flex-col items-center mb-10">
-                <div className="flex mt-5">
-                    <div className="w-[45%] mx-auto">
-                        <img src={company.imageUrl} />
-                    </div>
-                    <div className="w-[45%] mx-auto flex flex-col justify-center">
-                        <h1>Company Name: {company.name} </h1>
-                        <h1>Email: {company.email} </h1>
-                        <h1>CUIT: {formatedCuit(company.cuit)} </h1>
-                        <h1>Address: {company.address} </h1>
-                        <h1>Company Description: {company.description} </h1>
-                        <h1>Phone Number: {company.phoneNumber} </h1>
-                        {company.distance && <h1>Distance: {company.distance?.text} </h1>}
-                    </div>
-                    <div>
-                        <Map location={company.location} />
-                    </div>
-                </div>
+  if (!company.id) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div>
+      <div className=" mt-60 flex flex-col items-center">
+        <div className=" w-3/4 mt-4 mb-2 bg-slate-50 rounded-lg shadow-md flex">
+          <div className="flex h-full w-full flex-col ">
+            <div className="flex justify-center items-center mt-80">
+              <img
+                src={company.imageUrl}
+                className="flex top-32 absolute rounded-full w-[400px] h-[400px] shadow-xl"
+              />
             </div>
-            <Grid value="detail" />
+            <div className="w-full flex flex-col justify-center">
+              <div className=" flex flex-col gap-y-6">
+                <div className=" flex flex-row justify-center">
+                  <h1 className=" tracking-wide font-semibold text-5xl">
+                    {company.name}
+                  </h1>
+                </div>
+                <div>
+                  <h1 className=" tracking-wide font-semibold text-xl text-center mx-64 mt-4 ">
+                    {company.description}
+                  </h1>
+                </div>
+                <div className=" items-center justify-center flex flex-row gap-56 mt-4 ">
+                  <div>
+                    <h1 className=" tracking-wide font-bold text-lg text-center">
+                      Email
+                    </h1>
+                    <h1 className=" tracking-wide font-semibold text-base">
+                      {company.email}{" "}
+                    </h1>
+                  </div>
+                </div>
+                <div className=" items-center justify-center flex flex-row gap-56 mt-4 mb-10">
+                  <div>
+                    <h1 className=" tracking-wide font-bold text-lg text-center">
+                      Address
+                    </h1>
+                    <h1 className=" tracking-wide font-semibold text-base">
+                      {company.address}
+                    </h1>
+                  </div>
+                  <div>
+                    <h1 className=" tracking-wide font-bold text-lg text-center">
+                      Phone Number
+                    </h1>
+                    <h1 className=" tracking-wide font-semibold text-base">
+                      {company.phoneNumber}
+                    </h1>
+                  </div>
+                </div>
+                <div className=" flex flex-col items-center justify-center mb-16">
+                  {company.distance && (
+                    <h1>Distance {company.distance?.text}</h1>
+                  )}
+                  <div >
+                    <Map location={company.location} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+      <Grid value="detail"/>
+    </div>
+  );
 }
+
