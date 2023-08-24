e5bc1bc7640c572cd2c954853c3c3b3323b231d6
Luis Canales <canalesluis9@gmail.com>
2023-08-24 05:59:32 -0300

    authmid22

--- server/src/utils/authMiddleware.js
+++ server/src/utils/authMiddleware.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 async function verifyToken(req, res, next) {
   try {
     console.log(req.cookies.accessTrue)
-    if (!req.cookies.accessTrue && !req.headers.authorization) {
+    if (!req.cookies.accessTrue) {
       return res.status(406).json({ message: "Token not provided" });
     }
-    const { accessTrue } = req.cookies || req.headers.authorization.split(" ")[1];
+    const { accessTrue } = req.cookies;
     const { userId } = verify(accessTrue, process.env.JWT_SECRET)
     console.log(userId)
     req.userId = userId;
