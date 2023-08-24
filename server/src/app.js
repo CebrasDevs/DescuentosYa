229d0eac6443f0427c0bf3d6a43376179b3fa0b2
Luis Canales <129117019+SnowDevLC@users.noreply.github.com>
2023-08-24 03:11:15 -0300

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // Establecemos cabeceras de acceso CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://descuentos-ya.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "https://descuentos-ya.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
