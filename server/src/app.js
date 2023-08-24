94cbba9b11cea10771209c2056d39b230ca76541
Luis Canales <canalesluis9@gmail.com>
2023-08-24 04:29:41 -0300

    app

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // Establecemos cabeceras de acceso CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", `${process.env.URL_BASE}`);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
